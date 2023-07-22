46fe3c2adc0c019f1b1f8b4fc9afb8f381a3814e
Evan Hadam <hadame.eh@gmail.com>
2023-07-21 21:57:25 -0400

    cleaner ukutuner

--- ukutuner.py
+++ ukutuner.py
@@ -1,86 +1,65 @@
 import numpy as np
 import pyaudio
 
-# Feel free to play with these numbers. Change NOTE_MIN
-# and NOTE_MAX especially for guitar/bass. Keep
-# FRAME_SIZE and FRAMES_PER_FFT to be powers of two.
-
 NOTE_MIN = 60       # C4
 NOTE_MAX = 69       # A4
-FSAMP = 22050       # Sampling frequency in Hz
+FSAMP = 22050       # Sampling frequency(Hz)
 FRAME_SIZE = 2048   # Samples per frame
-FRAMES_PER_FFT = 16 # FFT takes average across FRAMES_PER_FFT frames
+FRAMES_PER_FFT = 32 # average across FRAMES_PER_FFT frames in FFT
+SAMPLES_PER_FFT = FRAME_SIZE*FRAMES_PER_FFT # SAMPLES_PER_FFT increases, frequency reads more accurately but takes longer
+FREQ_STEP = float(FSAMP)/SAMPLES_PER_FFT # frequency resolution
 
-# Derived quantities from constants above. Note that as
-# SAMPLES_PER_FFT goes up, the frequency step size decreases (so
-# resolution increases); however, it will incur more delay to process
-# new sounds.
-
-SAMPLES_PER_FFT = FRAME_SIZE*FRAMES_PER_FFT
-FREQ_STEP = float(FSAMP)/SAMPLES_PER_FFT
-
-# For printing out notes
+# low to high notes
 NOTE_NAMES = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
 
-######################################################################
-# These three functions are based upon this very useful webpage:
-# https://newt.phys.unsw.edu.au/jw/notes.html
+# Music theory - converting frequencies to numbers that correlate with notes
+# https://www.inspiredacoustics.com/en/MIDI_note_numbers_and_center_frequencies
 
-def freq_to_number(f): 
+def freqNum(f): 
     return 69 + 12*np.log2(f/440.0)
-def number_to_freq(n): 
+
+def noteFftbin(n): 
+    return numFreq(n)/FREQ_STEP
+
+def numFreq(n): 
     return 440 * 2.0**((n-69)/12.0)
-def note_name(n): 
+
+def noteName(n): 
     return NOTE_NAMES[n % 12] + str(n/12 - 1)
 
-######################################################################
+imin = max(0, int(np.floor(noteFftbin(NOTE_MIN-1))))
+imax = min(SAMPLES_PER_FFT, int(np.ceil(noteFftbin(NOTE_MAX+1))))
 
-# Get min/max index within FFT of our notes.
-# See docs for numpy.rfftfreq()
-def note_to_fftbin(n): return number_to_freq(n)/FREQ_STEP
-imin = max(0, int(np.floor(note_to_fftbin(NOTE_MIN-1))))
-imax = min(SAMPLES_PER_FFT, int(np.ceil(note_to_fftbin(NOTE_MAX+1))))
-
-# Allocate space to run an FFT. 
+# stores samples
 buf = np.zeros(SAMPLES_PER_FFT, dtype=np.float32)
 num_frames = 0
 
-# Initialize audio
-stream = pyaudio.PyAudio().open(format=pyaudio.paInt16,
-                                channels=1,
-                                rate=FSAMP,
-                                input=True,
-                                frames_per_buffer=FRAME_SIZE)
-
+stream = pyaudio.PyAudio().open(format=pyaudio.paInt16, channels=1, rate=FSAMP, input=True, frames_per_buffer=FRAME_SIZE)
 stream.start_stream()
 
-# Hanning Window
+# Hanning Window - helps cut down non-applicable local maxima and isolate our note frequency
 window = 0.5 * (1 - np.cos(np.linspace(0, 2*np.pi, SAMPLES_PER_FFT, False)))
 
-# Print initial text
-print('sampling at', FSAMP, 'Hz with max resolution of', FREQ_STEP, 'Hz')
-print
+print('Detecting notes now!')
 
-# As long as we are getting data:
 while stream.is_active():
 
-    # Shift the buffer down and new data in
+    # shift buffer for space for new data
     buf[:-FRAME_SIZE] = buf[FRAME_SIZE:]
     buf[-FRAME_SIZE:] = np.frombuffer(stream.read(FRAME_SIZE), np.int16)
 
-    # Run the FFT on the windowed buffer
+    # FFT w/ windowed buffer
     fft = np.fft.rfft(buf * window)
 
-    # Get frequency of maximum response in range
+    # Frequency of most common response
     freq = (np.abs(fft[imin:imax]).argmax() + imin) * FREQ_STEP
 
-    # Get note number and nearest note
-    n = freq_to_number(freq)
+    # Get note number, nearest note and their distance away from each other
+    n = freqNum(freq)
     n0 = int(round(n))
+    nDelta = n - n0
 
-    # Console output once we have a full buffer
     num_frames += 1
 
     if num_frames >= FRAMES_PER_FFT:
-        print('freq: {:7.2f} Hz     note: {:>3s} {:+.2f}'.format(
-            freq, note_name(n0), n-n0))
+        print('note: {:>3s} \t{:+.2f}'.format(noteName(n0), nDelta))
